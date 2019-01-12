5bd4530b476db958d12d6756833976d7e5523ef9
Matt McGovern <magumba@magumbasauce.com>
2019-01-11 19:27:44 -0600

    super basic console app for min of Hangfire usage.

---
+++ ConsoleMemoryStorage/ConsoleMemoryStorage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Hangfire;
+using Hangfire.MemoryStorage;
+
+namespace ConsoleMemoryStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GlobalConfiguration.Configuration.UseMemoryStorage();
+
+            using(var server = new BackgroundJobServer())
+            {
+                BackgroundJob.Enqueue(() => Console.WriteLine("Hello World!"));
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
